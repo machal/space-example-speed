7d90e8df470ce3ae18847316d010aa00e48cb048
Martin Michálek <michalek@shortcat.cz>
2015-05-19 11:14:27 +0200

    Gulp - Autoprefixer a CSSmin

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp'),
   less = require('gulp-less'),
   path = require('path'),
   rename = require('gulp-rename'),
-  sourcemaps = require('gulp-sourcemaps');
+  sourcemaps = require('gulp-sourcemaps'),
+  autoprefixer = require('gulp-autoprefixer'),
+  cssmin = require('gulp-cssmin');
 
 // CSS
 // ---
@@ -14,9 +16,26 @@ gulp.task('less', function () {
     .pipe(sourcemaps.init())
     .pipe(less())
     .pipe(rename('style.css'))
+    .pipe(autoprefixer({
+            browsers: ['last 3 versions', 'ios 6', 'ie 7', 'ie 8', 'ie 9'],
+            cascade: false
+    }))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./dist/css/'));
 });
+
+// CSS minifikace
+
+gulp.task('cssmin', function () {
+  return gulp.src('./dist/css/style.css')
+    .pipe(cssmin())
+    .pipe(rename({
+      extname: ".min.css"
+    }))
+    .pipe(gulp.dest('./dist/css/'));
+});
+
+
 
 gulp.task('default', function() {
 
