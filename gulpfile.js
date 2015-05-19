da33404184e5ba7a98c2bfc73d7ecde71758efc4
Martin Michálek <michalek@shortcat.cz>
2015-05-19 11:05:22 +0200

    Gulp - LESS kompilace

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+  less = require('gulp-less'),
+  path = require('path'),
+  rename = require('gulp-rename'),
+  sourcemaps = require('gulp-sourcemaps');
+
+// CSS
+// ---
+
+// LESS kompilace + SourceMaps
+
+gulp.task('less', function () {
+  return gulp.src('./src/less/index.less')
+    .pipe(sourcemaps.init())
+    .pipe(less())
+    .pipe(rename('style.css'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/css/'));
+});
 
 gulp.task('default', function() {
-  // place code for your default task here
+
 });
