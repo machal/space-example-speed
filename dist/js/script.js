ae12f1428463dc56a57d82f210da078df48e4071
Martin Michálek <michalek@shortcat.cz>
2015-11-23 11:24:03 +0100

    Nova dist verze JS

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery JavaScript Library v1.11.2
+ * jQuery JavaScript Library v1.11.3
  * http://jquery.com/
  *
  * Includes Sizzle.js
@@ -9,7 +9,7 @@
  * Released under the MIT license
  * http://jquery.org/license
  *
- * Date: 2014-12-17T15:27Z
+ * Date: 2015-04-28T16:19Z
  */
 
 (function( global, factory ) {
@@ -64,7 +64,7 @@ var support = {};
 
 
 var
-	version = "1.11.2",
+	version = "1.11.3",
 
 	// Define a local copy of jQuery
 	jQuery = function( selector, context ) {
@@ -569,7 +569,12 @@ jQuery.each("Boolean Number String Function Array Date RegExp Object Error".spli
 });
 
 function isArraylike( obj ) {
-	var length = obj.length,
+
+	// Support: iOS 8.2 (not reproducible in simulator)
+	// `in` check used to prevent JIT error (gh-2145)
+	// hasOwn isn't used here due to false negatives
+	// regarding Nodelist length in IE
+	var length = "length" in obj && obj.length,
 		type = jQuery.type( obj );
 
 	if ( type === "function" || jQuery.isWindow( obj ) ) {
@@ -12364,15 +12369,154 @@ return jQuery;
 
 }(window, document, jQuery));
 
-$(document).ready(function(){
+/*! Ajax-Include - v0.1.4 - 2015-03-16
+* http://filamentgroup.com/lab/ajax_includes_modular_content/
+* Copyright (c) 2015 @scottjehl, Filament Group, Inc.; Licensed MIT */
 
-    // Ovladani navigace na mobilech
-    $('.head-hamburger a').click(function(event) {
-       event.preventDefault();
-       $('#nav').toggle();
-    });
+(function( $, win, undefined ){
 
-    // Inicializace Fancyboxu
-    $('.fancybox').fancybox();
+	var AI = {
+		boundAttr: "data-ajax-bound",
+		interactionAttr: "data-interaction",
+		// request a url and trigger ajaxInclude on elements upon response
+		makeReq: function( url, els, isHijax ) {
+			$.get( url, function( data, status, xhr ) {
+				els.trigger( "ajaxIncludeResponse", [ data, xhr ] );
+			});
+		},
+		plugins: {}
+	};
 
-})
+	$.fn.ajaxInclude = function( options ) {
+		var urllist = [],
+			elQueue = $(),
+			o = {
+				proxy: null
+			};
+
+		// Option extensions
+		// String check: deprecated. Formerly, proxy was the single arg.
+		if( typeof options === "string" ){
+			o.proxy = options;
+		}
+		else {
+			o = $.extend( o, options );
+		}
+
+		// if it's a proxy, que the element and its url, if not, request immediately
+		function queueOrRequest( el ){
+			var url = el.data( "url" );
+			if( o.proxy && $.inArray( url, urllist ) === -1 ){
+				urllist.push( url );
+				elQueue = elQueue.add( el );
+			}
+			else{
+				AI.makeReq( url, el );
+			}
+		}
+
+		// if there's a url queue
+		function runQueue(){
+			if( urllist.length ){
+				AI.makeReq( o.proxy + urllist.join( "," ), elQueue );
+				elQueue = $();
+				urllist = [];
+			}
+		}
+
+		// bind a listener to a currently-inapplicable media query for potential later changes
+		function bindForLater( el, media ){
+			var mm = win.matchMedia( media );
+			function cb(){
+				queueOrRequest( el );
+				runQueue();
+				mm.removeListener( cb );
+			}
+			if( mm.addListener ){
+				mm.addListener( cb );
+			}
+		}
+
+		// loop through els, bind handlers
+		this.not( "[" + AI.boundAttr + "]").not("[" + AI.interactionAttr + "]" ).each(function( k ) {
+			var el = $( this ),
+				media = el.attr( "data-media" ),
+				methods = [ "append", "replace", "before", "after" ],
+				method,
+				url,
+				isHijax = false,
+				target = el.attr( "data-target" );
+
+			for( var ml = methods.length, i=0; i < ml; i++ ){
+				if( el.is( "[data-" + methods[ i ] + "]" ) ){
+					method = methods[ i ];
+					url = el.attr( "data-" + method );
+				}
+			}
+
+			if( !url ) {
+				// <a href> or <form action>
+				url = el.attr( "href" ) || el.attr( "action" );
+				isHijax = true;
+			}
+
+			if( method === "replace" ){
+				method += "With";
+			}
+
+			el.data( "method", method )
+				.data( "url", url )
+				.data( "target", target )
+				.attr( AI.boundAttr, true )
+				.each( function() {
+					for( var j in AI.plugins ) {
+						AI.plugins[ j ].call( this, o );
+					}
+				})
+				.bind( "ajaxIncludeResponse", function( e, data, xhr ){
+					var content = data,
+						targetEl = target ? $( target ) : el;
+
+					if( o.proxy ){
+						var subset = new RegExp("<entry url=[\"']?" + el.data("url") + "[\"']?>((?:(?!</entry>)(.|\n))*)", "gmi").exec(content);
+						if( subset ){
+							content = subset[1];
+						}
+					}
+
+					var filteredContent = el.triggerHandler( "ajaxIncludeFilter", [ content ] );
+
+					if( filteredContent ){
+						content = filteredContent;
+					}
+
+					if( method === 'replaceWith' ) {
+						el.trigger( "ajaxInclude", [ content ] );
+						targetEl[ el.data( "method" ) ]( content );
+					} else {
+						targetEl[ el.data( "method" ) ]( content );
+						el.trigger( "ajaxInclude", [ content ] );
+					}
+				});
+
+			// When hijax, ignores matchMedia, proxies/queueing
+			if ( isHijax ) {
+				AI.makeReq( url, el, true );
+			}
+			else if ( !media || ( win.matchMedia && win.matchMedia( media ).matches ) ) {
+				queueOrRequest( el );
+			}
+			else if( media && win.matchMedia ){
+				bindForLater( el, media );
+			}
+		});
+
+		// empty the queue for proxied requests
+		runQueue();
+
+		// return elems
+		return this;
+	};
+
+	win.AjaxInclude = AI;
+}( jQuery, this ));
