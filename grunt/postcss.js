859343d504c7769b43d202c141f0ec55a6a9f4be
Martin Michálek <michalek@shortcat.cz>
2018-02-10 15:36:20 +0100

    Grunt: Odstraneni balicku Pixrem

--- grunt/postcss.js
+++ grunt/postcss.js
@@ -9,10 +9,8 @@ module.exports = {
     options: {
       map: true,
       processors: [
-        // rem -> px fallback:
-        require('pixrem')({rootValue: 16}),
         // pridani vendor prefixu
-        require('autoprefixer')({browsers: ['last 3 versions', 'ios 6', 'ie 7', 'ie 8', 'ie 9']}),
+        require('autoprefixer')(),
         // media queries z px do em
         require('postcss-em-media-query')({ }),
         //  minifikace CSS
